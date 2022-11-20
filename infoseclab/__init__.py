d6a55ca180edbe80e990fdd4972ea43ff54c354e
Florian Tramer <tramer@florians-macbook-pro.home>
2022-11-20 15:02:40 +0100

    relative imports

--- infoseclab/__init__.py
+++ infoseclab/__init__.py
@@ -1,6 +1,1 @@
-
-from .defenses.defense import ResNet
-from .defenses.quality_scorer import ImageQualityScorer
-from .defenses.defense_jpeg import ResNetJPEG
-from .attacks.pgd import PGD
 from .data import ImageNet, EPSILON
---
+++ infoseclab/defenses/__init__.py
@@ -0,0 +1,3 @@
+from .defense import ResNet
+from .quality_scorer import ImageQualityScorer
+from .defense_jpeg import ResNetJPEG
