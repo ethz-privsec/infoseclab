b9fd5b17d84f7ca45bebf5a845bac4fe8faaad30
Edoardo Debenedetti <edoardo.debenedetti@inf.ethz.ch>
2022-11-30 00:14:37 +0100

    Add extension patch

--- setup_euler.sh
+++ setup_euler.sh
@@ -8,9 +8,17 @@ source .venv/bin/activate
 echo "Reinstalling Jupyter Lab inside of environment"
 python -m pip install jupyterlab --force-reinstall
 
-echo "Installing Jupyter extension to use Colab"
+echo "Installing Jupyter extension from pip to connect to Colab"
 python -m pip install jupyter_http_over_ws
+
+echo "Patching Jupyter extension"
+ORIGINAL_STRING="_AUTH_URL_QUERY_PARAM = 'jupyter_http_over_ws_auth_url'"
+SUBSTITUTE_STRING="_AUTH_URL_QUERY_PARAM = '127.0.0.1:8888'"
+FILE_TO_PATCH=".venv/lib64/python3.10/site-packages/jupyter_http_over_ws/handlers.py"
+sed -i -e "s/${ORIGINAL_STRING}/${SUBSTITUTE_STRING}/g" $FILE_TO_PATCH
+
+echo "Installing extension into Jupyter Lab"
 .venv/bin/jupyter serverextension enable --py jupyter_http_over_ws
 
-echo "Deactivating"
+echo "Setup finished, deactivating environment"
 deactivate
--- start_jupyter.sh
+++ start_jupyter.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-#SBATCH -n 4
-#SBATCH --time=4:00
+#SBATCH -n=4
+#SBATCH --time=4:00:00
 #SBATCH --mem-per-cpu=5G
 #SBATCH --job-name=jupyter
 #SBATCH --output=jupyter.out
 #SBATCH --error=jupyter.err
-#SBATCH --gpus=gtx_1080_ti:1
+#SBATCH --gpus=1
 
 module load gcc/8.2.0 python_gpu/3.10.4 hdf5/1.10.1 eth_proxy
 hostname -i
 .venv/bin/jupyter lab --no-browser \
     --NotebookApp.allow_origin='https://colab.research.google.com' \
     --port=8888 \
-    --ip $(hostname -i) \
+    --ip "0.0.0.0" \
+    --NotebookApp.disable_check_xsrf=True \
+    --allow-root \
     --NotebookApp.port_retries=0
